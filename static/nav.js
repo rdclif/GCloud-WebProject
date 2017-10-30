4f7d69207bd7b1f0dcd3604476476a59127f10d9
rdclif <cliftonr@oregonstate.edu>
2017-10-29 17:44:01 -0700

    updates and fixes to function

--- static/nav.js
+++ static/nav.js
@@ -1,4 +1,6 @@
+//jquery for nav bar function
 $(document).ready(function() {
+    //loading larger screens
     if($( window ).width() >= "600") {
         $('.narrow-hide').css("display", "block");
         $('.hamburger').hide();
@@ -6,6 +8,13 @@ $(document).ready(function() {
 
     }
 
+    //for loading very small screens
+    if($( window ).width() <= "350") {
+        $('.hamburger').hide();
+        $(".slide-nav").slideToggle("medium");
+    }
+
+    //click function for hamburger
     $('.hamburger').click(function () {
         $('.hamburger').toggleClass("change");
         $(".slide-nav").slideToggle("medium");
@@ -16,7 +25,9 @@ $(document).ready(function() {
         $(".slide-nav").slideToggle("medium");
     });
 
+    //adjust for window resizing
     $(window).resize(function() {
+        //for larger screens
         if($( window ).width() >= "600") {
             $('.narrow-hide').css("display", "block");
             $('.hamburger').hide();
@@ -26,9 +37,18 @@ $(document).ready(function() {
             }
 
         }
+        //for very small screens
+        else if($( window ).width() <= "350"){
+            $('.hamburger').hide();
+            $(".slide-nav").slideDown("medium");
+        }
         else {
             $('.narrow-hide').css("display", "none");
+            $(".slide-nav").slideUp("medium");
             $('.hamburger').show();
+            if($('.hamburger').hasClass("change")){
+                $('.hamburger').removeClass("change");
+            }
         }
     });
 
